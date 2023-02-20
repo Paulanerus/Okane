282beb3362a427f12475fe49545dd9915fa6fed4
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-02-20 21:10:47 +0100

    AppConfig now holds a YearEntry vector

--- src/config/AppConfig.hpp
+++ src/config/AppConfig.hpp
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "../entry/Entry.hpp"
+
 #include <string>
+#include <vector>
 
 struct AppConfig
 {
     std::string currency = "€";
+    std::vector<Okane::YearEntry> years;
 };
