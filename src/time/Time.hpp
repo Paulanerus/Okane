6928e9ad6daefcce2e2f21ed2223122ebda833c6
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-02-26 16:46:24 +0100

    Added getMonthFromId to map multiple month values to actual needed one

--- src/time/Time.hpp
+++ src/time/Time.hpp
@@ -3,11 +3,39 @@
 #include <ctime>
 #include <string>
 #include <sstream>
+#include <optional>
 #include <iomanip>
+#include <unordered_map>
 
 namespace Okane
 {
     const std::string DATE_FORMAT{"%d.%m.%Y"};
+
+    const std::unordered_map<std::string, std::vector<std::string>> MONTH_MAP = {
+        {"01", {"January", "Jan", "1", "01"}},
+        {"02", {"February", "Feb", "2", "02"}},
+        {"03", {"March", "Mar", "3", "03"}},
+        {"04", {"April", "Apr", "4", "04"}},
+        {"05", {"May", "5", "05"}},
+        {"06", {"June", "Jun", "6", "06"}},
+        {"07", {"July", "Jul", "7", "07"}},
+        {"08", {"August", "Aug", "8", "08"}},
+        {"09", {"September", "Sep", "9", "09"}},
+        {"10", {"October", "Oct", "10", "10"}},
+        {"11", {"November", "Nov", "11", "11"}},
+        {"12", {"December", "Dec", "12", "12"}},
+    };
+
+    inline std::optional<std::string> getMonthFromId(const std::string &id)
+    {
+        for (const auto &[month, ids] : MONTH_MAP)
+        {
+            if (std::find(ids.begin(), ids.end(), id) != ids.end())
+                return month;
+        }
+
+        return {};
+    }
 
     inline tm *getCurrentTime()
     {
