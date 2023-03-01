4890aad3f60ba27be95156a2b5af853e2cc8dd96
Paul <41129807+Paulanerus@users.noreply.github.com>
2023-03-01 20:19:05 +0100

    Fix for winows stuck on cout

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,20 @@
 #include <clocale>
 #include <iostream>
 
+#ifdef _WIN32
+#include "Windows.h"
+#endif
 int main(int argc, char **args)
 {
     if (argc <= 1)
         return EXIT_SUCCESS;
 
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    setlocale(LC_CTYPE, "");
+#else
     setlocale(LC_CTYPE, "en_US.utf8");
+#endif
 
     rang::setControlMode(rang::control::Force);
 
